cac5a9fe340c17ecc0dab2547f7b07fb8751f397
koownij <koownij@kakao.com>
2022-12-10 11:32:22 +0900

    first commit

---
+++ client/src/Main.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.net.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Thread receiveBroadCastSocket = new Thread(
+                () -> {
+                    /**
+                     * TODO 전체 코인 받아오는 스레드
+                     * 서버에서 코인 정보 갱신 brocastsocket.read를 통해 읽어오고
+                     * 읽어온 정보를 업데이트 시켜줌
+                     */
+                    try (MulticastSocket socket = new MulticastSocket(5000)) {
+                        InetAddress groupAddress = InetAddress.getByName("224.0.0.1");
+                        socket.joinGroup(groupAddress);
+                        byte[] buffer = new byte[1024 * 1024];
+                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length); // 연결 지향이 아니기때문에 받기만 하는 입장인 Client는 주소와 포트는 필요가 없다.
+                        while (true) {
+                            socket.receive(packet); // receive는 packet으로 받아야함
+                            byte[] receiveData = packet.getData();
+                            String data = new String(receiveData, 0, packet.getLength());
+                            System.out.println(data);
+                        }
+                    }
+                    catch (SocketException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+
+        receiveBroadCastSocket.start();
+
+        Thread receiveTCPSocket = new Thread(
+                () -> {
+                    /**
+                     * TODO 서버로부터 매수/매도 요청 받아오기
+                     * 받아온 정보 gui에 갱신
+                     */
+                    try {
+                        Socket clientSocket = new Socket(Inet4Address.getByName("127.0.0.1"), 4000);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+
+        receiveTCPSocket.start();
+    }
+}
---
+++ client/src/SimpleMultiTheadedServer.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.*;
+import java.text.NumberFormat;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SimpleMultiTheadedServer implements Runnable {
+
+    private static final ConcurrentHashMap<String, Float> map;
+    private final Socket clientSocket;
+
+    static {
+        map = new ConcurrentHashMap<>();
+        map.put("Axle", 238.50f);
+        map.put("Gear", 45.55f);
+        map.put("Wheel", 86.30f);
+        map.put("Rotor", 8.50f);
+    }
+
+    SimpleMultiTheadedServer(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Multi-Threaded Server Started");
+        try {
+            new Thread(
+                    () -> {
+                        try(DatagramSocket socket = new DatagramSocket()) {
+                            socket.setBroadcast(true);
+                            String message = "노트북 친구들 하이";
+                            byte[] buffer = message.getBytes();
+                            while(true) {
+                                try {
+                                    InetAddress groupAddress = InetAddress.getByName("224.0.0.1");
+                                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length, groupAddress, 5000);
+                                    socket.send(packet);
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            ).start();
+
+            ServerSocket serverSocket = new ServerSocket(4000);
+            while (true) {
+                System.out.println("Listening for a client connection");
+                Socket socket = serverSocket.accept();
+                System.out.println("Connected to a Client");
+                new Thread(new SimpleMultiTheadedServer(socket)).start();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Multi-Threaded Server Terminated");
+    }
+
+    public void run() {
+        System.out.println("Client Thread Started");
+        try (BufferedReader bis = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+                PrintStream out = new PrintStream(
+                        clientSocket.getOutputStream())) {
+
+            String partName = bis.readLine();
+            float price = map.get(partName);
+            out.println(price);
+            NumberFormat nf = NumberFormat.getCurrencyInstance();
+            System.out.println("Request for " + partName
+                    + " and returned a price of "
+                    + nf.format(price));
+            
+//            while(true) {
+//                String partName = bis.readLine();
+//                if("quit".equalsIgnoreCase(partName)) {
+//                    break;
+//                }
+//                float price = map.get(partName);
+//                out.println(price);
+//                NumberFormat nf = NumberFormat.getCurrencyInstance();
+//                System.out.println("Request for " + partName
+//                        + " and returned a price of "
+//                        + nf.format(price));
+//            } 
+            
+            clientSocket.close();
+            System.out.println("Client Connection Terminated");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Client Thread Terminated");
+    }
+}
